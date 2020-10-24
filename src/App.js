3f93f68a72b0809b88cb401f6725b358f5f0bc11
joevtap <joelvitortorres@gmail.com>
2020-10-24 12:58:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import StyledContentLoader from "styled-content-loader";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [personName, setPersonName] = useState(null);
+    const [personLastName, setPersonLastName] = useState(null);
+    const [personPicture, setPersonPicture] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://api.randomuser.me/");
+            const data = await response.json();
+            const name = data.results[0].name.first;
+            const lastName = data.results[0].name.last;
+            const picture = data.results[0].picture.large;
+            setPersonName(name);
+            setPersonLastName(lastName);
+            setPersonPicture(picture);
+        }
+        fetchData();
+    }, []);
+
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
+    if (personName !== null) {
+        return (
+            <div className="App">
+                <div className="card">
+                    <img
+                        className="profile-pic"
+                        src={personPicture}
+                        alt="profile"
+                    />
+                    <p className="name">{`${personName} ${personLastName}`}</p>
+                    <button className="refresh" onClick={refreshPage}>
+                        Refresh
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="App">
+            <StyledContentLoader backgroundColor="#1d2d50" foregroundColor="#133b5c">
+                <div className="card"/>
+            </StyledContentLoader>
+        </div>
+    );
 }
-
-export default App;
